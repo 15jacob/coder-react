8549f5e7c00bd811e9b5ed4871494c92419313ac
Jacob <15jacob.cd@gmail.com>
2022-03-21 04:42:28 -0300

    Correcciones

--- src/components/mainContainer.jsx
+++ src/components/mainContainer.jsx
@@ -6,7 +6,15 @@ function MainContainer()
     return (
         <div className="container-fluid h-100 justify-content-center" id="main-container">
             <ItemListContainer message="No hemos encontrado resultados para ese termino, o no estamos seguros de que estás buscando, probá con otra cosa"/>
-            <hr className="my-10"></hr>
+
+            <hr className="mt-50"></hr>
+
+            <div>
+                <h6 className="d-inline-block w-100 t-center">Muestra provisoria de ItemDetailContainer / ItemDetail</h6>
+            </div>
+
+            <hr className="mb-50" />
+
             <ItemDetailContainer/>
         </div>
     );
